b4a7b98afe25b80d1539021cbb80d73678ea17d7
G. Gordon Nasseri <ProphetManX@gmail.com>
2020-05-22 23:30:53 -0400

    created the database project with some basic schema and data

---
+++ ProphetsWay.Example.Database/PostBuildScripts/CreateCompanies.sql
@@ -0,0 +1,27 @@
+﻿USE [$(DatabaseInstance)]
+GO
+
+SET IDENTITY_INSERT dbo.Companies ON
+
+MERGE dbo.Companies AS Target
+USING (VALUES
+	(1, 'ACME', 'Great Products for a Great Price!'), 
+	(2, 'Dunder Mifflin', 'We sell Paper' )
+) AS Source (Id, Name, Other)
+	ON Target.Id = Source.Id
+WHEN MATCHED THEN
+	UPDATE SET
+		Name = Source.Name,
+		Other = Source.Other
+WHEN NOT MATCHED BY Target THEN 
+	INSERT (Id, Name, Other)
+	VALUES (
+		Source.Id, 
+		Source.Name, 
+		Source.Other
+	)
+WHEN NOT MATCHED BY Source THEN
+	DELETE;
+
+SET IDENTITY_INSERT dbo.Companies OFF
+
---
+++ ProphetsWay.Example.Database/PostBuildScripts/CreateUsers.sql
@@ -0,0 +1,34 @@
+﻿USE [$(DatabaseInstance)]
+GO
+
+SET IDENTITY_INSERT dbo.Users ON
+
+MERGE dbo.Users AS Target
+USING (VALUES 
+	(1, 'Bugs', 'What''s up Doc?', 1, 2, 'User', 1),
+	(2, 'Michael Scott', 'That''s what she said!', 2, 1, 'Admin', 0)	
+) AS Source (Id, Name, Whatever, CompanyId, JobId, RoleStr, RoleInt)
+	ON Target.Id = Source.Id
+WHEN MATCHED THEN
+	UPDATE SET
+		Name = Source.Name, 
+		Whatever = Source.Whatever,
+		CompanyId = Source.CompanyId, 
+		JobId = Source.JobId, 
+		RoleStr = Source.RoleStr,
+		RoleInt = Source.RoleInt
+WHEN NOT MATCHED BY Target THEN 
+	INSERT (Id, Name, Whatever, CompanyId, JobId, RoleStr, RoleInt)
+	VALUES (
+		Source.Id, 
+		Source.Name, 
+		Source.Whatever,
+		Source.CompanyId, 
+		Source.JobId, 
+		Source.RoleStr,
+		Source.RoleInt
+	)
+WHEN NOT MATCHED BY Source THEN
+	DELETE;
+
+SET IDENTITY_INSERT dbo.Users OFF
