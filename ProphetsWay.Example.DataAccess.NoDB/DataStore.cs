cb3e4ab66c3e5123d962a6c10b6ebf43fad5fe10
G. Gordon Nasseri <ProphetManX@gmail.com>
2020-05-24 18:11:11 -0400

    implemented a no database instance of the dataaccess project, and created a unit test projec to test/verify everything works

---
+++ ProphetsWay.Example.DataAccess.NoDB/DataStore.cs
@@ -0,0 +1,19 @@
+﻿using ProphetsWay.Example.DataAccess.Entities;
+using System.Collections.Generic;
+
+namespace ProphetsWay.Example.DataAccess.NoDB
+{
+	/// <summary>
+	/// Ignore this class, I created it to be an in-memory database for use in the example.
+	/// 
+	/// In your implementation, this could be considered your database context (ex: Entity Framework implementation)
+	/// </summary>
+	internal static class DataStore
+	{
+		public static readonly Dictionary<int, Job> Jobs = new Dictionary<int, Job>();
+
+		public static readonly Dictionary<int, Company> Companies = new Dictionary<int, Company>();
+
+		public static readonly Dictionary<int, User> Users = new Dictionary<int, User>();
+	}
+}
---
+++ ProphetsWay.Example.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ProphetsWay.Example.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
